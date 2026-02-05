2e87de85c9775f9f50f7547e419345b2db001949
Merchan-Anthony-2001 <anthony.merchana@ug.edu.ec>
2026-02-04 23:07:32 -0500

    Actualizacion de Dashboard

--- MS-Administracion/src/api/Components/dashboard_component.py
+++ MS-Administracion/src/api/Components/dashboard_component.py
@@ -3,24 +3,27 @@ from flask_restful import Resource
 from ..Services.dashboard_service import DashboardService
 from ...utils.general.logs import HandleLogs
 from ..Services.middleware import valida_api_token
+from datetime import date
+
 
 class DashboardComponent(Resource):
 
     @valida_api_token
     def get(self):
         """
-        Obtiene las métricas generales del dashboard filtradas por idcia.
-        Requiere token de seguridad.
+        Dashboard Adaptativo para los 3 Roles:
+        - Rol 1 (Admin) y Rol 2 (Gerente): No envían idcia, ven datos globales
+        - Rol 3 (Admin Sucursal): Envía idcia, ve ocupación por franjas .
         """
         try:
-            # Recibimos el idcia por parámetros para filtrar las métricas
+            # Capturamos el idcia (ahora es opcional para no bloquear al Gerente)
             id_cia = request.args.get('idcia')
-            if not id_cia:
-                return {"result": False, "message": "ID de compañía requerido para el dashboard"}, 400
 
-            # Si el token es válido, llamamos al Service para las métricas
+            # Llamamos al Service con los parámetros
             resultado = DashboardService.obtener_resumen(id_cia)
+
             return resultado, 200 if resultado['result'] else 500
+
         except Exception as e:
             HandleLogs.write_error(e)
             return {"result": False, "message": str(e)}, 500
